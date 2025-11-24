8df073158b55aa9fa4e86ead1e91c4afce3a8488
Tarun Verghis <tarun.verghis@gmail.com>
2025-11-23 18:29:27 -0800

    Handle sender and receiver connection messages
    When accepting a TCP connection, we expect the first message received to
    be of the form "{send|recv}|{room}". This allows the system to identify
    whether the client intends to connect as a sender or a reciever, and
    through which room it will be relaying data.
    
    If the first message is not of the expected form, the client is shooed
    away. We also never (currently) expect the receiver to send up anything
    past the initial identification message ("recv|foo-room"), so the client
    is shooed away in this case as well.

--- lib/vesper/ConnectionHandler.ex
+++ lib/vesper/ConnectionHandler.ex
@@ -1,15 +1,44 @@
 defmodule Vesper.ConnectionHandler do
   use ThousandIsland.Handler
 
+  require Logger
+  alias ThousandIsland.Socket
+
+  @goaway "GOAWAY\n"
+
   @impl ThousandIsland.Handler
-  def handle_connection(socket, state) do
-    ThousandIsland.Socket.send(socket, "Welcome!\n")
+  def handle_connection(_socket, _state) do
+    {:continue, %{role: :unknown}}
+  end
+
+  @impl ThousandIsland.Handler
+  def handle_data(data, socket, %{role: role} = state) when role == :unknown do
+    case String.trim(data) do
+      "send|" <> room when room != "" ->
+        Logger.debug("Established sender connection for room #{room}.")
+        {:continue, %{role: :sender, room: room}}
+      "recv|" <> room when room != "" ->
+        Logger.debug("Established receiver connection for room #{room}.")
+        {:continue, %{role: :receiver, room: room}}
+      _ ->
+        Logger.warning("Received invalid role message, closing connection.")
+        close_socket_rudely(socket, state)
+    end
+  end
+
+  @impl ThousandIsland.Handler
+  def handle_data(_data, _socket, %{role: role} = state) when role == :sender do
     {:continue, state}
   end
 
   @impl ThousandIsland.Handler
-  def handle_data(data, socket, state) do
-    ThousandIsland.Socket.send(socket, data)
-    {:continue, state}
+  def handle_data(_data, socket, %{role: role, room: room} = state) when role == :receiver do
+    Logger.warning("Received unexpected data from receiver for room #{room}, closing connection.")
+    close_socket_rudely(socket, state)
+  end
+
+  defp close_socket_rudely(socket, state) do
+    Socket.send(socket, @goaway)
+    {:close, state}
   end
 end
